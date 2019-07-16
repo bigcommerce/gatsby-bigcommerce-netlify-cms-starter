3594cac64d279bc23f2980eed35bd48521e0008f
Nate Stewart <nate.stewart@bigcommerce.com>
2019-07-16 07:16:32 -0500

    Add styles to add to cart button

--- src/components/AddToCartButton.js
+++ src/components/AddToCartButton.js
@@ -53,12 +53,21 @@ const AddToCartButton = class extends React.Component {
     const { currency, cartAmount, lineItems, numberItems, redirectUrls } = this.state.cart
 
     return (
-      <div>
-        {this.state.addingToCart ? (
-          <button class="bc-btn bc-btn--form-submit bc-btn--add_to_cart" type="submit">Adding to Cart...</button>
-        ) : (
-          <button class="bc-btn bc-btn--form-submit bc-btn--add_to_cart" type="submit" onClick={this.addToCart}>Add to Cart</button>
-        )}
+      <div className="bc-product__actions" data-js="bc-product-group-actions">
+        <div className="bc-form bc-product-form">
+          <div className="bc-product-form__product-message"></div>
+          
+          <button className="bc-btn bc-btn--form-submit bc-btn--add_to_cart" type="submit" onClick={this.addToCart}>Add to Cart</button>
+
+          {this.state.addingToCart ? (
+            <div className="bc-ajax-add-to-cart__message-wrapper">
+              <p className="bc-ajax-add-to-cart__message bc-alert bc-alert--success">
+                Product successfully added to your cart.
+                <a href="{ redirectUrls.checkout_url }">Proceed to Checkout</a>.
+              </p>
+            </div>
+          ) : ()}
+        </div>
       </div>
     )
   }
