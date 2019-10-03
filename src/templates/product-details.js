81667e5eb5e9a560905374642c2ce9f944ccd36a
Grant Glidewell <grant@thirdandgrove.com>
2019-10-03 12:43:25 -0700

    adding to cart from product page

--- src/templates/product-details.js
+++ src/templates/product-details.js
@@ -2,14 +2,41 @@ import React from 'react';
 import { graphql } from 'gatsby';
 
 import AddToCartButton from '../components/AddToCartButton';
+import Layout from '../components/Layout';
 
-export default ({ data }) => (
-  <div className="content">
-    <h1>product</h1>
-    <h3>tap tap tap, is this thing on??</h3>
-    <pre>{JSON.stringify(data, null, 2)}</pre>
-  </div>
-);
+export default ({
+  data: {
+    allBigCommerceProducts: { nodes }
+  }
+}) => {
+  const { name, id, sku, price, description, variants, images } = nodes[0];
+  return (
+    <Layout>
+      <div className="content">
+        <div className="has-text-centered margin-top-0">
+          <h1
+            className="has-text-weight-bold is-size-1"
+            style={{
+              boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
+              backgroundColor: '#f40',
+              color: 'white',
+              padding: '1rem'
+            }}>
+            {name}
+          </h1>
+        </div>
+        <section className="section">
+          <div className="container">
+            <img src={images[0]} alt="" />
+            <div className="content">{description}</div>
+            <AddToCartButton productId={id} variantId={variants[0].id} />
+          </div>
+        </section>
+        <h3>tap tap tap, is this thing on??</h3>
+      </div>
+    </Layout>
+  );
+};
 
 export const query = graphql`
   query($productId: String!) {
