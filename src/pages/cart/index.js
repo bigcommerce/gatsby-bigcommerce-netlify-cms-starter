e234a4d60127a81954e5d73f67ea7f02600c593f
Nate Stewart <nate.stewart@bigcommerce.com>
2019-10-14 03:50:36 -0500

    Moved cart into bigcommerce component folder

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Layout from '../../components/Layout'
-import Cart from '../../components/Cart'
+import Cart from '../../components/bigcommerce/Cart'
 
 export default class CartIndexPage extends React.Component {
   render() {
