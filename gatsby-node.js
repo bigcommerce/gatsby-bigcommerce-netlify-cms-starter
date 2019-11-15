12aec105962627aa841ccc7da34b78a70c9b67bf
Nate Stewart <nate.stewart@bigcommerce.com>
2019-11-15 13:52:46 -0600

    Initial commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,41 @@
+require('dotenv').config();
+const axios = require('axios');
 const _ = require('lodash');
 const path = require('path');
 const { createFilePath } = require('gatsby-source-filesystem');
 const { fmImagesToRelative } = require('gatsby-remark-relative-images');
+
+async function fetchChannels() {
+  return await axios({
+    url: `https://api.bigcommerce.com/stores/${process.env.API_STORE_HASH}/v3/channels`,
+    method: 'get',
+    headers: {
+      'X-Auth-Token': process.env.API_TOKEN,
+      'X-Auth-Client': process.env.API_CLIENT_ID,
+      'Content-Type': 'application/json'
+    }
+  }).then((result) => {
+    return result.data.data;
+  }).catch(function (error) {
+    console.log(error);
+  });
+}
+
+async function fetchChannelListings(channelID) {
+  return await axios({
+    url: `https://api.bigcommerce.com/stores/${process.env.API_STORE_HASH}/v3/channels/${channelID}/listings`,
+    method: 'get',
+    headers: {
+      'X-Auth-Token': process.env.API_TOKEN,
+      'X-Auth-Client': process.env.API_CLIENT_ID,
+      'Content-Type': 'application/json'
+    }
+  }).then((result) => {
+    return result.data.data;
+  }).catch(function (error) {
+    console.log(error);
+  });
+}
 
 exports.createPages = async ({ actions, graphql }) => {
   const { createPage } = actions;
@@ -22,9 +56,10 @@ exports.createPages = async ({ actions, graphql }) => {
           }
         }
       }
-      allBigCommerceProducts {
+      allBigCommerceProducts(limit: 100) {
         nodes {
           id
+          bigcommerce_id
           name
           custom_url {
             url
@@ -42,15 +77,38 @@ exports.createPages = async ({ actions, graphql }) => {
   const posts = result.data.allMarkdownRemark.edges;
   const products = result.data.allBigCommerceProducts.nodes;
 
-  products.forEach(({ custom_url, id }) => {
-    createPage({
-      path: `/products${custom_url.url}`,
-      component: path.resolve(`src/templates/product-details.js`),
-      context: {
-        productId: id
+  console.log('fetching channels');
+  const channels = await fetchChannels();
+
+  for (var i = channels.length - 1; i >= 0; i--) {
+    let channel = channels[i];
+
+    if (channel.is_enabled) {
+      console.log(`creating product pages for channel: ${channel.name}`);
+      let channelListings = await fetchChannelListings(channel.id);
+
+      for (var x = channelListings.length - 1; x >= 0; x--) {
+        let channelListing = channelListings[x];
+        // console.log(`product_id: ${channelListing.product_id} listing_id: ${channelListing.listing_id}`);
+
+        if (channelListing.state === "active") {
+          products.forEach( ({ bigcommerce_id, custom_url, id }) => {
+            if (bigcommerce_id === channelListing.product_id) {
+              console.log(`${channel.external_id}/products${custom_url.url}`);
+              createPage({
+                path: `${channel.external_id}/products${custom_url.url}`,
+                component: path.resolve(`src/templates/product-details.js`),
+                context: {
+                  productId: id,
+                  overrides: channelListing.overrides || {}
+                }
+              });
+            }
+          });
+        }
       }
-    });
-  });
+    }
+  }
 
   posts.forEach(edge => {
     const id = edge.node.id;
