e521e9b75fa9d163c6100afa0c70f33d4f3566ee
Nate Stewart <nate.stewart@bigcommerce.com>
2019-11-20 04:56:03 -0600

    Remove product page CMS preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,7 +4,6 @@ import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerMediaLibrary(uploadcare);
@@ -12,5 +11,4 @@ CMS.registerMediaLibrary(cloudinary);
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
