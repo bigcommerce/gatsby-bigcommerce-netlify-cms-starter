2c86e442dc50c7ab8e942f4900845501783799d0
Nate Stewart <nate.stewart@bigcommerce.com>
2019-11-25 14:38:30 -0600

    Logging frog BlogItem preview

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -2,14 +2,18 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { BlogPostTemplate } from '../../templates/blog-post'
 
-const BlogPostPreview = ({ entry, widgetFor }) => (
-  <BlogPostTemplate
-    content={widgetFor('body')}
-    description={entry.getIn(['data', 'description'])}
-    tags={entry.getIn(['data', 'tags'])}
-    title={entry.getIn(['data', 'title'])}
-  />
-)
+const BlogPostPreview = ({ entry, widgetFor }) => {
+  console.log(entry)
+  console.log(widgetFor)
+  return (
+    <BlogPostTemplate
+      content={widgetFor('body')}
+      description={entry.getIn(['data', 'description'])}
+      tags={entry.getIn(['data', 'tags'])}
+      title={entry.getIn(['data', 'title'])}
+    />
+  )
+}
 
 BlogPostPreview.propTypes = {
   entry: PropTypes.shape({
